d2537e5d45709bb85054fb9097ae2999ab18dec3
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-06-01 13:44:10 +0800

    Implemented proper seeders

--- app/Models/NVPModel.php
+++ app/Models/NVPModel.php
@@ -2,13 +2,13 @@
 
 namespace App\Models;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 // use Illuminate\Database\Eloquent\Model;
 use Jenssegers\Mongodb\Eloquent\Model;
 
 class NVPModel extends Model
 {
-    // use HasFactory;
+    use HasFactory;
 
     protected $collection = 'name-value-pairs';
     protected $fillable = ['key', 'value'];
