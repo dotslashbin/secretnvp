36ca1f14ec39ebb2f93f850403aabfd08b1f1ab5
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-06-01 18:25:48 +0800

    Added helpers to generate output format

--- app/Helpers/NVPHelpers.php
+++ app/Helpers/NVPHelpers.php
@@ -1,5 +1,29 @@
 <?php
 
-function ConvertTimeStamp(string $timeStamp) {
-	return "this isthe converted";
+/**
+ * Generates a formatted structure with the data
+ *
+ * @param [type] $data
+ * @param integer $page
+ * @param integer $itemsPerPage
+ * @return void
+ */
+function FormatReturn($data, int $page = 1, int $itemsPerPage = 10) {
+	$format = new stdClass;
+
+	$format->data = $data;
+	$format->page = $page; 
+	$format->itemsPerPage = $itemsPerPage;
+
+	return $format;
+}
+
+/**
+ * Generates the JSON response for the api
+ *
+ * @param [type] $data
+ * @return void
+ */
+function GenerateResponse($data) {
+	return response()->json($data)->header('Content-Type', 'application/json');
 }
