b2e1c4f138e7a432b7eb23c673b5e17ed1fdaebd
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-06-01 18:27:24 +0800

    Implemented reader service

---
+++ app/Services/NVPServices/NVPReader.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services\NVPServices;
+use App\Services\NVPServices\DataServiceINT;
+use App\Models\NVPModel;
+
+
+class NVPReader implements DataServiceINT {
+
+	private $filterKey;
+	private $filterTimestamp;
+
+	function __construct(string $key = '', string $timestamp = '')
+	{
+		$this->filterKey = $key;
+		$this->filterTimestamp = $timestamp;
+	}
+
+	/**
+	 * Executes the running of db query
+	 *
+	 * @return void
+	 */
+	public function RunQuery()
+	{
+		$query = NVPModel::where('key', $this->filterKey);
+
+		if($this->filterTimestamp) {
+			$query->where('value', 'velit'); // TODO change this to the real one
+		}
+
+		return $query->get();
+	}
+}
