e0a60237cbee09d2efb66dd36cbd801d2a0712cc
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-06-01 13:25:54 +0800

    Added test fetch

--- app/Http/Controllers/NVPReadController.php
+++ app/Http/Controllers/NVPReadController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use stdClass;
+
+use App\Models\NVPModel;
 
 class NVPReadController extends Controller
 {
@@ -14,6 +17,15 @@ class NVPReadController extends Controller
      */
     public function __invoke(Request $request)
     {
-        exit(var_dump($request));
+
+
+        $sampleData = new stdClass;
+
+        $sampleData->key = $request->key;
+
+        $test = NVPModel::find('60b59a9478dad54a3b8b3ca6');
+        $sampleData->test = $test;
+        
+        return response()->json($sampleData)->header('Content-Type', 'application/json');
     }
 }
