d3bfa2a95c7fe55fedaa2d74f28898de6104f11c
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-06-01 09:51:15 +0800

    Renamed controller and references to be more appropriate

--- app/Http/Controllers/NVPReadController.php
+++ app/Http/Controllers/NVPReadController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class NVPFetchController extends Controller
+class NVPReadController extends Controller
 {
     /**
      * Handle the incoming request.
