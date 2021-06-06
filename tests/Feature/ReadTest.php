57b7e7f0e009e09d1a5a8962901e30d15607d6ca
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-06-06 14:29:23 +0800

    Added test for "get all" without paginaiton

--- tests/Feature/ReadTest.php
+++ tests/Feature/ReadTest.php
@@ -8,6 +8,8 @@ use Tests\TestCase;
 
 class ReadTest extends TestCase
 {
+    const API_PATH = '/api/object';
+
     public function test_if_active_route() {
         $response = $this->get('/api/object/test');
         $response->assertStatus(200);
@@ -21,9 +23,23 @@ class ReadTest extends TestCase
     public function test_if_the_resulting_key_are_the_same()
     {
         $inputValue = 'FOO';
-        $content = $this->get('/api/object/'.$inputValue)->getContent();
+        $response = $this->get(self::API_PATH.'/'.$inputValue);
+        $response->assertStatus(200);
+        $content = $response->getContent(); 
 
         $result = json_decode($content);
         $this->assertTrue($result->data->key === $inputValue);
+    }
+
+    /**
+     * Test to see if pagination works when there are no parameters given. 
+     * @return void
+     */
+    public function test_to_see_if_calling_to_get_all_without_parameters_returns_a_paginated_result()
+    {
+        $response = $this->get(self::API_PATH.'/get-all-records');
+        $response->assertStatus(200);
+        $content = json_decode($response->getContent());
+        $this->assertTrue(count($content->data) <= config('app.DEFAULT_ITEMS_PER_PAGE'));
     }
 }
