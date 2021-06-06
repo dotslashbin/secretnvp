6776357193923c1caf23117b60f524b5dfa09a80
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-06-06 18:15:12 +0800

    See if this passes

--- tests/Feature/ReadTest.php
+++ tests/Feature/ReadTest.php
@@ -12,18 +12,4 @@ class ReadTest extends TestCase
         $response = $this->get('/api/object/test');
         $response->assertStatus(200);
     }
-
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_if_the_resulting_key_are_the_same()
-    {
-        $inputValue = 'FOO';
-        $content = $this->get('/api/object/'.$inputValue)->getContent();
-
-        $result = json_decode($content);
-        $this->assertTrue($result->data->key === $inputValue);
-    }
 }
