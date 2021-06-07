9c62384360a27f64f178316cfcbcde06ad80d09a
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-06-07 22:34:51 +0800

    Implemented middleware to convert json into the wanted  format

---
+++ app/Http/Middleware/InputConverter.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\VarDumper\VarDumper;
+
+class InputConverter
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $expectedJSON = $request->all();
+        if(!is_array($expectedJSON)) {
+            return new JsonResponse('Invalid input format', 422);
+        }
+
+        foreach($expectedJSON as $inputtedKey => $inputtedValue) {
+            $request->merge([ "key" => (string) $inputtedKey, "value" => (string)$inputtedValue]);
+        }
+
+        return $next($request);
+    }
+}
