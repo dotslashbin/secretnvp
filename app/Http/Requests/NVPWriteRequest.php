2c4d72ef45966ca46022fc559743800729c64351
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-06-07 23:36:21 +0800

    Implemented call for formatting

--- app/Http/Requests/NVPWriteRequest.php
+++ app/Http/Requests/NVPWriteRequest.php
@@ -35,6 +35,7 @@ class NVPWriteRequest extends FormRequest
     }
 
     protected function failedValidation(Validator $validator) {
-        throw new HttpResponseException(response()->json($validator->errors(), 422));
+        $output = FormatReturn($validator->errors(), config('app.NVPReturnStructures.ERROR'));
+        throw new HttpResponseException(response()->json($output, 422));
     }
 }
